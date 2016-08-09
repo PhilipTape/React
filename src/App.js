16b43a2b49ed8f930f88de9d6ac5a14cceb4286e
PhilipTape <philoxovo@gmail.com>
2016-08-09 19:02:55 +0800

    React-app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+
+    name:""
+  };
+  
+  utrohon = (event)=> {
+    
+    var inputName = event.target.value;
+    
+  this.setState({
+    name: inputName   
+ });
+}; 
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Whats up! Welcome to The World of React</h2>
+          <h2>Welcome to The World of React - {this.state.name}</h2>
         </div>
         <p className="App-intro">
-        <h3>Lets start to explore with react!</h3>
+        <form>
+        Enter your name:
+        <input type="text" onChange={this.utrohon}/>
+        </form>
         </p>
       </div>
     );
